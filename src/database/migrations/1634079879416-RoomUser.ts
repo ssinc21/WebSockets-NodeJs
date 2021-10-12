dd185732b038d4556c8b4f9d4cec124f1e58a7d1
Inácio Silva <inacio.silva.1999@gmail.com>
2021-10-12 20:05:40 -0300

    Criando Migrations para inserir as informações no BD

---
+++ src/database/migrations/1634079879416-RoomUser.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class RoomUser1634079879416 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { serverHttp } from "./http";
+import "reflect-metadata"
 import "./websocket";
+import "./database"
 
 serverHttp.listen(3000, () => console.log("Server is running on PORT 3000, WebSockets"));
